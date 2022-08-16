59f08174b56db9ff9804caf94c69d81e8859e1e8
Zihan Zhao <zihan0932@gmail.com>
2022-08-16 09:18:41 -0700

    Optimize animation and update documentations

---
+++ scripts/selection.js
@@ -0,0 +1,37 @@
+interval = 0;
+
+const selectionSort = function () {
+  // we're gonna use i outside of the loop for coloring
+  for (var i = 0; i < arraySize - 1; i++) {
+    var minIdx = i;
+
+    // mark the current min index position until swap
+    animation(bars[i], barValue[i], "#ff6b6b", ++interval);
+    for (let j = i + 1; j < arraySize; j++) {
+      // mark the current visiting bar
+      animation(bars[j], barValue[j], "#ff6b6b", ++interval);
+      if (barValue[j] < barValue[minIdx]) {
+        minIdx = j;
+      }
+      // restore bar color after visiting
+      animation(bars[j], barValue[j], "#96f2d7", ++interval);
+    }
+
+    // defined in bubble sort
+    swap(barValue, i, minIdx);
+
+    // mark the two bars that will be swapped
+    animation(bars[minIdx], barValue[minIdx], "#ffd43b", ++interval);
+    animation(bars[i], barValue[i], "#ffd43b", interval);
+
+    // restore color for the bar that was previously at the min index position
+    animation(bars[minIdx], barValue[minIdx], "#96f2d7", ++interval);
+
+    // mark the min index position node as completed
+    animation(bars[i], barValue[i], "#228be6", ++interval);
+  }
+
+  // eventually the loop ended where i === n - 1
+  animation(bars[i], barValue[i], "#228be6", ++interval);
+  interval = 0;
+};
