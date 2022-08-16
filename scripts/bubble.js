70342d20b289141d905c597d44ced1c43fe06a72
Zihan Zhao <zihan0932@gmail.com>
2022-08-16 12:34:11 -0700

    Optimize visualization color

--- scripts/bubble.js
+++ scripts/bubble.js
@@ -9,10 +9,11 @@ const bubbleSort = function () {
   for (let i = 0; i < arraySize - 1; i++) {
     // we're gonna use j outside of the loop for coloring
     for (var j = 0; j < arraySize - i - 1; j++) {
+      animation(bars[j], barValue[j], "#ff6b6b", interval);
       if (barValue[j] > barValue[j + 1]) {
         // mark bars that we are going to swap
-        animation(bars[j], barValue[j], "#ff6b6b", ++interval);
-        animation(bars[j + 1], barValue[j + 1], "#ff6b6b", interval);
+        animation(bars[j], barValue[j], "#ffd43b", ++interval);
+        animation(bars[j + 1], barValue[j + 1], "#ffd43b", interval);
 
         swap(barValue, j, j + 1);
 
