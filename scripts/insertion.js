1645dde54274261f64a618b47d48d8d4a6c57614
Zihan Zhao <zihan0932@gmail.com>
2022-08-17 07:36:28 -0700

    Optimize animation and documentations

--- scripts/insertion.js
+++ scripts/insertion.js
@@ -1,44 +1,55 @@
+"use strict";
+
 interval = 0;
 
 const insertionSort = function () {
   for (let i = 1; i < arraySize; i++) {
-    // keep a record of the current bar we want to insert, will lose reference later
+    // mark the current bar we want to insert
     const key = barValue[i];
     animation(bars[i], barValue[i], "#ff6b6b", interval);
 
     let j = i - 1;
 
+    // if the key value is greater than its previous bar value
+    // then we are at the correct position to insert the key
+    if (barValue[i] >= barValue[j]) {
+      // mark this index is the correct position with color yellow
+      animation(bars[i], key, "#ffd43b", ++interval);
+
+      // if the key is not at the initial position which is i === 1
+      // we can be sure all its previous bars have been sorted
+      // then just change color for key itself
+      if (i !== 1) {
+        animation(bars[i], key, "#228be6", ++interval);
+      } else {
+        // else mark all previous bars including the key as sorted
+        for (let k = j; k <= i; k++) {
+          animation(bars[k], barValue[k], "#228be6", ++interval);
+        }
+      }
+
+      // no further operations required, continue the outer for-loop
+      continue;
+    }
+
     while (j >= 0 && barValue[j] > key) {
       // traverse back
-      animation(bars[j], barValue[j], "#ff6b6b", ++interval);
+      animation(bars[j], key, "#ff6b6b", ++interval);
 
+      // move bars with higher value one step backwards while we traverse
       barValue[j + 1] = barValue[j];
-      // mark visited, notice following steps will override values including key
-      if (j === i - 1) {
-        /*****   IMPORTANT   *****/
-        // correct way to display, since insertion sort overwrite values including key values
-        // animation(bars[j + 1], barValue[j], "#ff6b6b", interval);
-
-        // but I kept the key value as height for better visualization
-        animation(bars[j + 1], key, "#ff6b6b", interval);
-      } else {
-        animation(bars[j + 1], barValue[j], "#228be6", interval);
-      }
+      animation(bars[j + 1], barValue[j], "#228be6", interval);
       j--;
     }
-    // we find the position to insert the key
-    animation(bars[j + 1], barValue[j + 1], "#ffd43b", interval);
 
+    // we found the correct position to insert
+    // all values after this point is greater than the key value
     barValue[j + 1] = key;
 
-    // at the previous key position, the value has been overridden
-    // mark this position with the correct key value again to notify insertion
-    animation(bars[i], key, "#ffd43b", interval++);
-
-    // this loop is only for marking sorted area
-    for (let k = 0; k <= i; k++) {
-      animation(bars[k], barValue[k], "#228be6", interval);
-    }
+    // mark position with color yellow to notify it is the position for insertion
+    animation(bars[j + 1], key, "#ffd43b", ++interval);
+    // mark position with color blue to show completion of insertion
+    animation(bars[j + 1], key, "#228be6", ++interval);
   }
   interval = 0;
 };
