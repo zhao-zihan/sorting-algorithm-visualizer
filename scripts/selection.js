616c166abaf4803d8396bd3417bd1f1d761b6a4d
Zihan Zhao <zihan0932@gmail.com>
2022-08-17 22:12:13 -0700

    Optimize color choices for selection sort

--- scripts/selection.js
+++ scripts/selection.js
@@ -1,3 +1,12 @@
+/**
+ * #ffd43b min value position
+ * #ff6b6b traversing pointer
+ * #15aabf current min value
+ * #da77f2 swap required
+ * #69db7c swap completed
+ * #228be6 node sorted
+ */
+
 "use strict";
 
 interval = 0;
@@ -13,22 +22,27 @@ const selectionSort = function () {
       // mark the current visiting bar
       animation(bars[j], barValue[j], "#ff6b6b", interval++);
       if (barValue[j] < barValue[minIdx]) {
+        if (minIdx !== i) {
+          animation(bars[minIdx], barValue[minIdx], "#96f2d7", interval);
+        }
         minIdx = j;
+        animation(bars[minIdx], barValue[minIdx], "#15aabf", interval);
+        continue;
       }
       // restore bar color after visiting
       animation(bars[j], barValue[j], "#96f2d7", interval);
     }
 
     // mark the two bars that will be swapped
-    animation(bars[minIdx], barValue[minIdx], "#ffd43b", interval++);
-    animation(bars[i], barValue[i], "#ffd43b", interval);
+    animation(bars[minIdx], barValue[minIdx], "#da77f2", interval);
+    animation(bars[i], barValue[i], "#da77f2", interval++);
 
     // defined in bubble sort
     swap(barValue, i, minIdx);
 
     // display swapped result
-    animation(bars[i], barValue[i], "#ffd43b", interval);
-    animation(bars[minIdx], barValue[minIdx], "#ffd43b", interval++);
+    animation(bars[i], barValue[i], "#69db7c", interval);
+    animation(bars[minIdx], barValue[minIdx], "#69db7c", interval++);
 
     // restore swapped new min index position bar color
     // need to do this step first then mark completed with blue
